16415b181248f14eb620d39ec4fc8fd90ced038c
Safat Hossain <thesafat@gmail.com>
2022-03-14 14:31:42 +0600

    basic keyboard input(keyListener) added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,17 +2,42 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GamePanel extends JPanel {
 
-    public GamePanel(){
+    int x=100,y=100;
 
+    public GamePanel(){
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {}
+
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                int e = keyEvent.getKeyCode();
+                if(e == KeyEvent.VK_UP) y -= 5;
+                if(e == KeyEvent.VK_DOWN) y += 5;
+                if(e == KeyEvent.VK_LEFT) x -= 5;
+                if(e == KeyEvent.VK_RIGHT) x += 5;
+
+            }
+
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {
+
+            }
+
+        });
     }
 
-    public void paintComponent(Graphics g){
+    @Override
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.drawRect(100, 100, 50, 50);
-    }
+        g.fillRect(x,y,50,50);
 
+        repaint();
+    }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -2,12 +2,14 @@ package main;
 
 import javax.swing.*;
 
-public class GameWindow {
+class GameWindow{
     JFrame jframe;
+
     public GameWindow(GamePanel gamePanel){
         jframe = new JFrame();
         jframe.setSize(400, 400);
         jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setLocationRelativeTo(null);
         jframe.add(gamePanel);
         jframe.setVisible(true);
     }
