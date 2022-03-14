059d5b592ef9cefa1a7b97d43ff2787e49d95c7f
Safat Hossain <thesafat@gmail.com>
2022-03-14 12:10:31 +0600

    first commit

---
+++ src/main/GameWindow.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.*;
+
+public class GameWindow {
+    JFrame jframe;
+    public GameWindow(GamePanel gamePanel){
+        jframe = new JFrame();
+        jframe.setSize(400, 400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.add(gamePanel);
+        jframe.setVisible(true);
+    }
+}
