942e799c15d734456ace53c20ad803995c51bc5e
Safat Hossain <thesafat@gmail.com>
2022-03-17 17:30:31 +0600

    random color added

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -7,25 +7,59 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 public class GamePanel extends JPanel {
 
-    public int x=100,y=100;
-    private MouseInputs mouseInputs;
+    public double x=100,y=10;
+    private double xDir=0.01, yDir=0.01;
+    //private MouseInputs mouseInputs;
+    private int frame = 0;
+    private long lastCheck = 0;
+    private Color color = new Color(100, 200, 100);
+    private Random random;
 
     public GamePanel(){
-        mouseInputs = new MouseInputs(this);
+        //mouseInputs = new MouseInputs(this);
         addKeyListener(new KeyboardInputs(this));
-        addMouseListener(mouseInputs);
-        addMouseMotionListener(mouseInputs);
+        //addMouseListener(mouseInputs);
+        //addMouseMotionListener(mouseInputs);
+        random = new Random();
     }
 
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        g.fillRect(x,y,50,50);
+        g.setColor(color);
 
+        updateRectangle();
+
+        g.fillRect((int)x, (int)y,50,50);
         repaint();
+        frame++;
+        if(System.currentTimeMillis() - lastCheck > 1000) {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frame);
+            frame = 0;
+        }
+
     }
+
+    void updateRectangle(){
+        x += xDir;
+        y += yDir;
+
+        if(x>350 || x<0) {
+            xDir *= -1;
+            color = new Color(random.nextInt(255), random.nextInt(255),random.nextInt(255));
+        }
+        if(y>350 || y<0) {
+            yDir *= -1;
+            color = new Color(random.nextInt(255), random.nextInt(255),random.nextInt(255));
+        }
+    }
+
 }
+
+
